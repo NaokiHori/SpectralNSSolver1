a763a052a8e9a7d40bab7b0c7455334b8908235d
NaokiHori <36466440+NaokiHori@users.noreply.github.com>
2024-07-01 11:24:43 +0000

    Extract 3d sources

--- src/domain.c
+++ src/domain.c
@@ -47,22 +47,16 @@ static int init_wave_numbers(
 ){
   int * restrict * xwaves = &domain->x1_xwaves;
   int * restrict * ywaves = &domain->x1_ywaves;
-#if NDIMS == 3
   int * restrict * zwaves = &domain->x1_zwaves;
-#endif
   *xwaves = memory_calloc(domain->s_x1_mysizes[0], sizeof(int));
   *ywaves = memory_calloc(domain->s_x1_mysizes[1], sizeof(int));
-#if NDIMS == 3
   *zwaves = memory_calloc(domain->s_x1_mysizes[2], sizeof(int));
-#endif
   int * restrict allwaves[NDIMS] = {
     *xwaves,
     *ywaves,
-#if NDIMS == 3
     *zwaves,
-#endif
   };
-  // compute wave numbers | 17
+  // compute wave numbers 
   // wave numbers
   //   0, 1, ..., N/2-1, -N/2, -N/2+1, ..., -2, -1
   // e.g. glsize = 8
@@ -88,34 +82,24 @@ static int init_angular_frequency(
 ){
   int * restrict xwaves = domain->x1_xwaves;
   int * restrict ywaves = domain->x1_ywaves;
-#if NDIMS == 3
   int * restrict zwaves = domain->x1_zwaves;
-#endif
   double * restrict * xfreqs = &domain->x1_xfreqs;
   double * restrict * yfreqs = &domain->x1_yfreqs;
-#if NDIMS == 3
   double * restrict * zfreqs = &domain->x1_zfreqs;
-#endif
   *xfreqs = memory_calloc(domain->s_x1_mysizes[0], sizeof(double));
   *yfreqs = memory_calloc(domain->s_x1_mysizes[1], sizeof(double));
-#if NDIMS == 3
   *zfreqs = memory_calloc(domain->s_x1_mysizes[2], sizeof(double));
-#endif
   const int * restrict allwaves[NDIMS] = {
     xwaves,
     ywaves,
-#if NDIMS == 3
     zwaves,
-#endif
   };
   double * restrict allfreqs[NDIMS] = {
     *xfreqs,
     *yfreqs,
-#if NDIMS == 3
     *zfreqs,
-#endif
   };
-  // compute angular frequency | 9
+  // compute angular frequency 
   for(size_t dim = 0; dim < NDIMS; dim++){
     const double length = domain->lengths[dim];
     const size_t mysize = domain->s_x1_mysizes[dim];
@@ -133,41 +117,24 @@ int domain_init(
     domain_t * domain
 ){
   // decompose domain
-#if NDIMS == 2
-  if(0 != sdecomp.construct(MPI_COMM_WORLD, NDIMS, (size_t [NDIMS]){0, 0}, (bool [NDIMS]){true, true}, &domain->info)){
-    printf("%s:%d domain decomposition failed\n", __FILE__, __LINE__);
-    return 1;
-  }
-#else
   if(0 != sdecomp.construct(MPI_COMM_WORLD, NDIMS, (size_t [NDIMS]){0, 0, 0}, (bool [NDIMS]){true, true, true}, &domain->info)){
     printf("%s:%d domain decomposition failed\n", __FILE__, __LINE__);
     return 1;
   }
-#endif
   // load parameters
   if(0 != load(dirname, domain)){
     return 1;
   }
   // global domain size, spectral domain
-#if NDIMS == 2
-  domain->s_glsizes[0] = domain->p_glsizes[0];
-  domain->s_glsizes[1] = domain->p_glsizes[1] / 2 + 1;
-#else
   domain->s_glsizes[0] = domain->p_glsizes[0];
   domain->s_glsizes[1] = domain->p_glsizes[1];
   domain->s_glsizes[2] = domain->p_glsizes[2] / 2 + 1;
-#endif
   // local coordinate
   for(size_t dim = 0; dim < NDIMS; dim++){
     sdecomp.get_pencil_mysize(domain->info, SDECOMP_X1PENCIL, dim, domain->s_glsizes[dim], &domain->s_x1_mysizes[dim]);
     sdecomp.get_pencil_offset(domain->info, SDECOMP_X1PENCIL, dim, domain->s_glsizes[dim], &domain->s_x1_offsets[dim]);
-#if NDIMS == 2
-    sdecomp.get_pencil_mysize(domain->info, SDECOMP_Y1PENCIL, dim, domain->p_glsizes[dim], &domain->p_y1_mysizes[dim]);
-    sdecomp.get_pencil_offset(domain->info, SDECOMP_Y1PENCIL, dim, domain->p_glsizes[dim], &domain->p_y1_offsets[dim]);
-#else
     sdecomp.get_pencil_mysize(domain->info, SDECOMP_Z1PENCIL, dim, domain->p_glsizes[dim], &domain->p_z1_mysizes[dim]);
     sdecomp.get_pencil_offset(domain->info, SDECOMP_Z1PENCIL, dim, domain->p_glsizes[dim], &domain->p_z1_offsets[dim]);
-#endif
   }
   init_wave_numbers(domain);
   init_angular_frequency(domain);
